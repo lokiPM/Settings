445e23358eb082b0f79daa59a9543a8e9a66591c
lubro <142106680+lokiPM@users.noreply.github.com>
2025-03-13 16:25:33 +0100

    Added CPS Popup

---
+++ src/Main.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace lokiPM\Settings;
+
+use pocketmine\plugin\PluginBase;
+use pocketmine\command\Command;
+use pocketmine\command\CommandSender;
+use pocketmine\player\Player;
+use pocketmine\form\Form;
+use pocketmine\event\Listener;
+use pocketmine\event\player\PlayerChatEvent;
+use pocketmine\event\player\PlayerMoveEvent;
+use pocketmine\event\player\PlayerJoinEvent;
+use pocketmine\event\player\PlayerQuitEvent;
+use pocketmine\event\server\DataPacketReceiveEvent;
+use pocketmine\network\mcpe\protocol\InventoryTransactionPacket;
+use pocketmine\network\mcpe\protocol\types\inventory\UseItemOnEntityTransactionData;
+use pocketmine\utils\Config;
+use pocketmine\scheduler\Task;
+
+class Main extends PluginBase implements Listener {
+
+    private $toggleState = [];
+    private $muteChat = [];
